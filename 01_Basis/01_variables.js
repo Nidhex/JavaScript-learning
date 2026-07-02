dd647bc69a767ea58a354fc816816d8390a82c4c
Nidhex <nidsvaishnav10@gmail.com>
2026-07-02 17:11:11 +0530

    basis of java script

---
+++ 01_Basis/01_variables.js
@@ -0,0 +1,23 @@
+const accounId = 132344322
+let accountEmail = "nidsvaishnav10@gmail.com"
+var accountPassword = "123456789"
+ accountCity = "Udaipur"
+let accountState;
+
+//accounId = 2 //not allowed
+
+console.log(accounId);
+
+/* 
+prefer not to use var
+    because of issue in block scope and functional scope
+*/
+
+accountEmail = "sjkddf@gmail.com"
+accountPassword = "21221222"
+accountCity = "goa"
+
+console.table([accounId,accountEmail,accountPassword,accountCity,accountState])
+
+
+
---
+++ 01_Basis/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer virsion
+
+// alert(3+3) // we are using node js,not browser
+
+let name ="NIDHSIH";
+let age = 18
+let isLoggIn = false 
+let state;
+
+//number==> 2 to power 53
+// bigint 
+//string 
+// boolean==> true/false
+// null => standlone value 
+// undefined =>
+// symbol => unique 
+
+
+// object
+
+console.log(typeof"nidds")
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
+
+                     
