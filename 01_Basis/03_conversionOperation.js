52e648790a53af29c9402a85eebc6154298e9315
Nidhex <nidsvaishnav10@gmail.com>
2026-07-02 17:37:55 +0530

    learn about datatype conversion

---
+++ 01_Basis/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score = "weryj"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueinnumber = Number(score)
+console.log(typeof valueinnumber);
+console.log(valueinnumber);
+
+// "33"=> 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let inlooggedoin = 1 
+let booleanisloggedin = Boolean(inlooggedoin);
+console.log(booleanisloggedin)
+
+// 1=>true; 0=>false
+// ""  => false
+// "nidsss " => true
+
+let someNumber = 88 
+let StringNumber =  String(someNumber)
+console.log( typeof StringNumber)
+console.log(  StringNumber)
